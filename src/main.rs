518290d1e72b43a1b582a45c23cc946047e109a3
zzeroo <co@zzeroo.com>
2024-02-03 22:03:27 +0100

    Create Github Action workflow files
    Shameless stolen from [wry](https://github.com/tauri-apps/wry) project

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,12 @@ struct Cli {
     maximized: bool,
 
     // Cycle time between site reloads (if more then one URL was given), in seconds
-    #[arg(long, short, default_value_t = 10, help = "Cycle time between site reloads (if more then one URL was given), in seconds")]
+    #[arg(
+        long,
+        short,
+        default_value_t = 10,
+        help = "Cycle time between site reloads (if more then one URL was given), in seconds"
+    )]
     cycle_sec: u64,
 }
 
@@ -65,39 +70,39 @@ fn main() -> wry::Result<()> {
         target_os = "android"
     )))]
     let builder = {
-    use tao::platform::unix::WindowExtUnix;
-    use wry::WebViewBuilderExtUnix;
-    let vbox = window.default_vbox().unwrap();
-    WebViewBuilder::new_gtk(vbox)
+        use tao::platform::unix::WindowExtUnix;
+        use wry::WebViewBuilderExtUnix;
+        let vbox = window.default_vbox().unwrap();
+        WebViewBuilder::new_gtk(vbox)
     };
-    
+
     let webview = builder.with_url(&start_url)?.build()?;
 
     let timer_length = Duration::new(cli.cycle_sec, 0);
 
     event_loop.run(move |event, _, control_flow| {
-      // If we have only one url no control flows (timers and such) are required
-      // this disables the match event block below complete.
-      if num_urls == 1 {
-        *control_flow = ControlFlow::Wait;
-      }
+        // If we have only one url no control flows (timers and such) are required
+        // this disables the match event block below complete.
+        if num_urls == 1 {
+            *control_flow = ControlFlow::Wait;
+        }
 
-      match event {
-        Event::NewEvents(StartCause::Init) => {
-            *control_flow = ControlFlow::WaitUntil(Instant::now() + timer_length)
-        }
-        Event::NewEvents(StartCause::ResumeTimeReached { .. }) => {
-            *control_flow = ControlFlow::WaitUntil(Instant::now() + timer_length);
-            if num_urls > 1 {
-                let url = urls.next().unwrap();
-                webview.load_url(&url);
+        match event {
+            Event::NewEvents(StartCause::Init) => {
+                *control_flow = ControlFlow::WaitUntil(Instant::now() + timer_length)
             }
+            Event::NewEvents(StartCause::ResumeTimeReached { .. }) => {
+                *control_flow = ControlFlow::WaitUntil(Instant::now() + timer_length);
+                if num_urls > 1 {
+                    let url = urls.next().unwrap();
+                    webview.load_url(&url);
+                }
+            }
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => (),
         }
-        Event::WindowEvent {
-            event: WindowEvent::CloseRequested,
-            ..
-        } => *control_flow = ControlFlow::Exit,
-        _ => (),
-      }
     });
 }
