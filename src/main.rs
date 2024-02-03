e41d9e500f427f780faa530db85a454d1423e341
Stefan Müller <stefan.mueller@hitachiastemo.com>
2024-02-03 17:24:23 +0100

    Handles multipe URLs now
    Additional parameter for the cycle timeout added.

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,84 @@
 // SPDX-License-Identifier: GPLv2
 
+use clap::Parser;
+use instant::Instant;
+use std::time::Duration;
 use tao::{
-  event::{Event, WindowEvent},
-  event_loop::{ControlFlow, EventLoop},
-  window::WindowBuilder,
+    event::{Event, StartCause, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
 };
 use wry::WebViewBuilder;
-use clap::Parser;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
-    /// Optional url to open, default https://google.com
-    #[arg(default_value = "https://google.com")]
-    url: String,
-    
+    /// Optional url(s) to open,
+    #[arg(default_values_t = vec!["https://google.com".to_string()])]
+    urls: Vec<String>,
+
     // Open window in fullscreen
-    #[arg(long, short, group = "options")]
+    #[arg(long, short, help = "Open window in fullscreen", group = "options")]
     fullscreen: bool,
-    
+
     // Open window maximized
-    #[arg(long, short, group = "options")]
+    #[arg(long, short, help = "Open window maximized", group = "options")]
     maximized: bool,
+
+    // Cycle time between site reloads (if more then one URL was given), in seconds
+    #[arg(long, short, default_value_t = 10, help = "Cycle time between site reloads (if more then one URL was given), in seconds", group = "options")]
+    cycle_sec: u64,
 }
 
 fn main() -> wry::Result<()> {
-  let cli = Cli::parse();
+    let cli = Cli::parse();
 
-  let event_loop = EventLoop::new();
-  let window = WindowBuilder::new().build(&event_loop).unwrap();
-  window.set_title("[Astemo] PublicViewing - Cli");
-  
-  if cli.maximized {
-    window.set_maximized(true);
-  }
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    window.set_title("[Astemo] PublicViewing - Cli");
 
-  if cli.fullscreen {
-    use tao::window::Fullscreen;
-    window.set_fullscreen(Some(Fullscreen::Borderless(None)));
-  }
-   
-  let builder = WebViewBuilder::new(&window);
+    let num_urls = cli.urls.len();
+    let mut urls = cli.urls.clone().into_iter().cycle();
+    let start_url = urls.next().unwrap();
 
-
-
-  let _webview = builder
-    .with_url(&cli.url)?
-    .build()?;
-  
-  event_loop.run(move |event, _, control_flow| {
-    *control_flow = ControlFlow::Wait;
-
-    if let Event::WindowEvent {
-      event: WindowEvent::CloseRequested,
-      ..
-    } = event
-    {
-      *control_flow = ControlFlow::Exit
+    if cli.maximized {
+        window.set_maximized(true);
     }
-  });
+
+    if cli.fullscreen {
+        use tao::window::Fullscreen;
+        window.set_fullscreen(Some(Fullscreen::Borderless(None)));
+    }
+
+    let builder = WebViewBuilder::new(&window);
+
+    let webview = builder.with_url(&start_url)?.build()?;
+
+    let timer_length = Duration::new(cli.cycle_sec, 0);
+
+    event_loop.run(move |event, _, control_flow| {
+      // If we have only one url no control flows (timers and such) are required
+      // this disables the match event block below complete.
+      if num_urls == 1 {
+        *control_flow = ControlFlow::Wait;
+      }
+
+      match event {
+        Event::NewEvents(StartCause::Init) => {
+            *control_flow = ControlFlow::WaitUntil(Instant::now() + timer_length)
+        }
+        Event::NewEvents(StartCause::ResumeTimeReached { .. }) => {
+            *control_flow = ControlFlow::WaitUntil(Instant::now() + timer_length);
+            if num_urls > 1 {
+                let url = urls.next().unwrap();
+                webview.load_url(&url);
+            }
+        }
+        Event::WindowEvent {
+            event: WindowEvent::CloseRequested,
+            ..
+        } => *control_flow = ControlFlow::Exit,
+        _ => (),
+      }
+    });
 }
